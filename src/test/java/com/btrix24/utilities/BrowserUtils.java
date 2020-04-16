5042bff1ea1e4b6810b7f73d10de6ea7f0123422
mestipule <mestipul@gmail.com>
2020-04-16 13:49:40 -0400

    report is added

--- src/test/java/com/btrix24/utilities/BrowserUtils.java
+++ src/test/java/com/btrix24/utilities/BrowserUtils.java
@@ -1,7 +1,13 @@
 package com.btrix24.utilities;
 
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebElement;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.*;
 
 public class BrowserUtils {
@@ -23,5 +29,18 @@ public class BrowserUtils {
 
         return stringList;
 
+    }
+    public static String getScreenshot(String name) throws IOException {
+        // name the screenshot with the current date time to avoid duplicate name
+        String date = new SimpleDateFormat("yyyyMMddhhmmss").format(new Date());
+        // TakesScreenshot ---> interface from selenium which takes screenshots
+        TakesScreenshot ts = (TakesScreenshot) Driver.getDriver();
+        File source = ts.getScreenshotAs(OutputType.FILE);
+        // full path to the screenshot location
+        String target = System.getProperty("user.dir") + "/test-output/Screenshots/" + name + date + ".png";
+        File finalDestination = new File(target);
+        // save the screenshot to the path given
+        FileUtils.copyFile(source, finalDestination);
+        return target;
     }
 }

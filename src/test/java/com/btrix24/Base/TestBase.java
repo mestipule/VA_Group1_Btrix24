cc738f28086e780da462ea1e010a9ed3d87a2e74
charossharipova <charos_sharipova@yahoo.com>
2020-04-17 13:57:35 -0400

    Charos 08

--- src/test/java/com/btrix24/Base/TestBase.java
+++ src/test/java/com/btrix24/Base/TestBase.java
@@ -7,17 +7,13 @@ import com.btrix24.pages.LoginPage;
 import com.btrix24.utilities.BrowserUtils;
 import com.btrix24.utilities.ConfigurationReader;
 import com.btrix24.utilities.Driver;
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
 import org.testng.ITestResult;
-
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
-import org.testng.asserts.SoftAssert;
 
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
---
+++ src/test/java/com/btrix24/tests/charos_tests.java
@@ -0,0 +1,47 @@
+package com.btrix24.tests;
+
+import com.btrix24.Base.TestBase;
+import com.btrix24.pages.LoginPage;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class charos_tests  extends TestBase {
+   // WebDriver driver;
+    LoginPage loginPage;
+    WebDriverWait wait;
+
+    @BeforeMethod
+    public void setUp() {
+        wait = new WebDriverWait(driver,10);
+    }
+
+    @Test
+    public void TopicIcon() throws InterruptedException {
+        test = report.createTest("Btrix24 Upload File Icon Functionality");
+        test.info("Opening Browser");
+        loginPage = new LoginPage();
+        test.info("Username input");
+        loginPage.username.sendKeys("helpdesk1@cybertekschool.com");
+        test.info("Password input");
+        loginPage.password.sendKeys("UserUser");
+        test.info("Logging in");
+        loginPage.login.click();
+        WebElement message = driver.findElement(By.id("feed-add-post-form-tab-message"));
+        message.click();
+        WebElement topicIcon = driver.findElement(By.id("lhe_button_title_blogPostForm"));
+        topicIcon.click();
+        Thread.sleep(3000);
+        WebElement topicmessage = driver.findElement(By.xpath("//div[@class='feed-add-post-title']/input[@id='POST_TITLE']"));
+        wait.until(ExpectedConditions.visibilityOf(topicmessage));
+        topicmessage.getAttribute("placeholder");
+        Assert.assertEquals(topicmessage.getAttribute("placeholder"), "Topic");
+
+
+    }
+}
